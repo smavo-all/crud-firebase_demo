e28b06749c6f0a100fb59e47c453529f7da0f11a
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-12 01:06:11 -0500

    add SweetAlert2

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,48 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroeModel } from 'src/app/models/heroe.model';
+
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { HeroeModel } from 'src/app/models/heroe.model';
 import { HeroesService } from 'src/app/services/heroes.service';
 
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  styleUrls: ['./heroe.component.css'],
 })
 export class HeroeComponent implements OnInit {
-
   heroe: HeroeModel = new HeroeModel();
-  
-  constructor(private heroesService: HeroesService) { }
 
-  ngOnInit(): void {
-  }
+  constructor(private heroesService: HeroesService) {}
 
-  guardar( form: NgForm ) {
+  ngOnInit(): void {}
 
-    if ( form.invalid ) {
+  guardar(form: NgForm) {
+    if (form.invalid) {
       console.log('Formulario no válido');
       return;
     }
 
-    
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false,
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
 
     if ( this.heroe.id ) {
-      this.heroesService.actualizarHeroe( this.heroe )
-      .subscribe(resp => {
-        console.log(resp);
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
+    } else {
+      peticion = this.heroesService.crearHeroe( this.heroe );
+    }
+
+    peticion.subscribe( resp => {
+      //console.log(resp);
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        icon: 'success'
+      });
+
     });
 
-    } else {
-     this.heroesService.crearHeroe( this.heroe )
-     .subscribe(resp => {
-      console.log(resp);
-      this.heroe = resp;
-  });
-
-    }
     //console.log(form);
     //console.log(this.heroe);
-    };
+  }
 
 }
