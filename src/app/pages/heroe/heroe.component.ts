68c0976bc3f6292765ba4ecb3bb44ba9cca8e2b0
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-12 00:23:23 -0500

    add http - post [crear nuevo heroe]

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { NgForm } from '@angular/forms';
+import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -11,7 +12,7 @@ export class HeroeComponent implements OnInit {
 
   heroe: HeroeModel = new HeroeModel();
   
-  constructor() { }
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
@@ -23,8 +24,13 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
-    console.log(form);
-    console.log(this.heroe);
+    //console.log(form);
+    //console.log(this.heroe);
+
+    this.heroesService.crearHeroe( this.heroe )
+    .subscribe(resp =>{
+        console.log(resp);
+    });
 
     };
 
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { HeroeModel } from '../models/heroe.model';
+import { map } from 'rxjs/operators';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroesService {
+  private url = 'https://crud-firebase-proyect-8172e.firebaseio.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  crearHeroe(heroe: HeroeModel) {
+    return this.http.post(`${this.url}/Heroes.json`, heroe)
+    .pipe(
+      map((resp: any) => {
+        heroe.id = resp.name;
+        return heroe;
+      })
+    );
+  }
 }
