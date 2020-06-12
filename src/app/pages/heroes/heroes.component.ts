5c5cb087baef4c874e11a4d0998b6ef2cc50e1a4
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-12 01:28:58 -0500

    add http - get [Listar heroes]

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { HeroeModel } from 'src/app/models/heroe.model';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: HeroeModel[] = [];
+
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
+
+    this.heroesService.getHeroes()
+      .subscribe(resp => {
+      this.heroes = resp
+      console.log(resp);
+    });
+
   }
+
+
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { HeroeModel } from '../models/heroe.model';
-import { map } from 'rxjs/operators';
+import { map , delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -24,14 +24,32 @@ export class HeroesService {
 
   
   actualizarHeroe( heroe: HeroeModel ) {
-
     const heroeTemp = {
       ...heroe //toma cada una de las propiedadaes y crea una con el nombre 
     };
-
     delete heroeTemp.id;
     return this.http.put(`${ this.url }/Heroes/${ heroe.id }.json`, heroeTemp);
   }
 
+  
+  getHeroes() {
+    return this.http.get(`${ this.url }/Heroes.json`)
+            .pipe(
+              map( this.crearArreglo )
+            );
+  }
+
+  private crearArreglo( heroesObj: object ) {
+    const heroes: HeroeModel[] = [];
+
+    Object.keys( heroesObj ).forEach( key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+      heroes.push( heroe );
+    });
+    return heroes;
+  }
+
+  
 
 }
