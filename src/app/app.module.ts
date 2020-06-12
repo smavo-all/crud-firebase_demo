ba98c37d4b5983781ec93e210a45035209407faf
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-11 23:56:32 -0500

    models/heroe.model.ts , manejo de formulario heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms'
+
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesComponent } from './pages/heroes/heroes.component';
@@ -14,7 +16,8 @@ import { HeroesComponent } from './pages/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/heroe.model.ts
@@ -0,0 +1,14 @@
+
+export class HeroeModel {
+
+    id: string;
+    nombre: string;
+    poder: string;
+    activo: boolean;
+
+    constructor() {
+        this.activo = true;
+    }
+
+}
+
